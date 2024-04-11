65d28fd24ef4dc17ec82a9360d59d77ff7f4abb2
Derek Kraan <derek@kraan.dev>
2024-04-11 15:15:00 +0200

    Bump version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HighlanderPg.MixProject do
   def project do
     [
       app: :highlander_pg,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       hex: [
