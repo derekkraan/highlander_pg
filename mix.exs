da2ec2950f4bd867e7d60dba715561fe342cde0a
Derek Kraan <derek@kraan.dev>
2024-07-08 12:16:13 +0200

    Also allow postgrex 0.18.x

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule HighlanderPg.MixProject do
 
   defp deps do
     [
-      {:postgrex, "~> 0.16.1 or ~> 0.17.0"},
+      {:postgrex, "~> 0.16.1 or ~> 0.17.0 or ~> 0.18.0"},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
